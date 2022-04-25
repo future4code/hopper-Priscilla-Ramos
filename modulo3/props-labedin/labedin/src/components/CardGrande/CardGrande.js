890c9d782f865183ad0207649e3715547cffbf03
PriscillaLucena <prilucenaoliveira@gmail.com>
2022-04-25 01:49:55 -0300

    fiz stylled components no card grande e no button

--- modulo3/props-labedin/labedin/src/components/CardGrande/CardGrande.js
+++ modulo3/props-labedin/labedin/src/components/CardGrande/CardGrande.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import './CardGrande.css'
+import styled from 'styled-components'
 
 function CardGrande(props) {
+
+    const BigCardContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 200px;
+    `
+
+    const BigCardImage = styled.img`
+     width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+    `
+
+    const BigCardH4 = styled.h4`
+    margin-bottom: 15px;
+    `
+
+    const BigCardDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+    `
+
     return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
-                <p>{ props.descricao }</p>
-            </div>
-        </div>
+        <BigCardContainer>
+            <BigCardImage src={props.imagem} />
+            <BigCardDiv>
+                <BigCardH4>{props.nome}</BigCardH4>
+                <p>{props.descricao}</p>
+            </BigCardDiv>
+        </BigCardContainer>
     )
 }
 
