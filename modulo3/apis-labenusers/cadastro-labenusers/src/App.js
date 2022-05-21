c0a51320869debce5d0717571a9462e783581412
PriscillaLucena <prilucenaoliveira@gmail.com>
2022-05-20 22:41:17 -0300

    fiz funções para os inputs e função post e delete, galta arrumar o delete e a troca de páginas

--- modulo3/apis-labenusers/cadastro-labenusers/src/App.js
+++ modulo3/apis-labenusers/cadastro-labenusers/src/App.js
@@ -1,12 +1,136 @@
 import React from 'react';
+import axios from 'axios';
 import Cadastro from './components/Cadastro';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 3vh;
+  margin-top: 5vh;
+`
 
 class App extends React.Component {
+
+  state = {
+    cadastros: [],
+    valorInputName: "",
+    valorInputEmail: ""
+  }
+
+  //joga o valor do input para o estado//
+
+  onChangeUserName = (event) => {
+    this.setState({ valorInputName: event.target.value });
+    console.log(this.state.valorInputName)
+  }
+
+  onChangeUserEmail = (event) => {
+    this.setState({ valorInputEmail: event.target.value });
+  }
+
+
+  //traz informações da API sobre o usuário//
+
+  pegaUser = () => {
+
+    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', {
+      headers: {
+        Authorization: "priscilla-ramos-hopper"
+      }
+    })
+      .then((response) => {
+        this.setState({ cadastros: response.data })
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+      })
+
+  }
+
+  //renderiza os usuários cadastrados na API na tela na inicialização da mesma//
+
+  componentDidMount = () => {
+    this.pegaUser()
+  }
+
+
+  //cadastra novo usuário na API//
+
+  cadastraUser = () => {
+
+    const body = {
+      name: this.state.valorInputName,
+      email: this.state.valorInputEmail
+    }
+
+    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body, {
+      headers: {
+        Authorization: "priscilla-ramos-hopper"
+      }
+    })
+      .then(() => {
+        this.setState({ valorInputName: '' })
+        this.setState({ valorInputEmail: '' })
+        this.pegaUser();
+        alert('Usuário criado com sucesso!')
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+        alert('Erro: não foi possivel criar!')
+      })
+  }
+
+  //deleta usuário da Api//
+
+  deletaUser = () => {
+
+    axios.delete("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/:id", {
+      headers: {
+        Authorization: "priscilla-ramos-hopper"
+      }
+    })
+      .then(() => {
+        alert('Usuário exluído com sucesso!')
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+        alert('Erro: não foi possivel excluir!')
+      })
+  }
+
+  renderizaNaTela = () => {
+    return <Cadastro
+      valorInputName={this.state.valorInputName}
+      onChangeUserName={this.onChangeUserName}
+      valorInputEmail={this.state.valorInputEmail}
+      onChangeUserEmail={this.onChangeUserEmail}
+      cadastraUser={this.cadastraUser}
+    />
+  }
+
+
+
+
   render() {
+
+    const listaCadastro = this.state.cadastros.map((cad) => {
+      return <li key={cad.id}>
+        {cad.name}{' '}
+        <button onClick={this.deletaUser}>x</button>
+      </li>
+
+    })
+
     return (
-      <div>
-        <Cadastro/>
-      </div>
+      <MainContainer>
+
+        {this.renderizaNaTela()}
+        {listaCadastro}
+
+
+      </MainContainer>
     );
   }
 }
--- modulo3/apis-labenusers/cadastro-labenusers/src/components/Cadastro.js
+++ modulo3/apis-labenusers/cadastro-labenusers/src/components/Cadastro.js
@@ -3,91 +3,24 @@ import axios from 'axios'
 
 class Cadastro extends React.Component {
 
-    state = {
-        cadastros: [],
-    valorInputName: '',
-    valorInputEmail: ''
-    }
-
-    componentDidMount = () => {
-        this.pegaUser()
-    }
-
-    // addUser = () =>{
-    //     const newUser = {
-    //         name: this.state.valorInputName,
-    //         email: this.state.valorInputEmail
-    //     }
-
-    //     const newUsers = [...this.cadastro, newUser]
-
-    //     this.setState({cadastros: newUsers})
-    // }
-
-    // onChangeUserName = (event) => {
-    //    this.setState({ valorInputName: event.target.value });
-    // }
-
-    // onChangeUserEmail = (event) => {
-    //    this.setState({ valorInputEmail: event.target.value });
-    // }
-
-    pegaUser = () => {
-
-        const body = {
-            id: "",
-            name: ""
-        }
-
-        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', body, {
-            headers: {
-                Authorization: "priscilla-lucena-hopper"
-            }
-        })
-        .then((response) => {
-            this.setState({cadastros: response.data.result.list })
-        })
-        .catch((error) => {
-            console.log(error.response.data)
-        })
-
-    }
-
-
-    cadastraUser = () => {
-   
-       const body = {
-            name: '',
-            email: ''
-        }
-
-        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body, {
-            headers: {
-                Authorization: "priscilla-ramos-hopper"
-            }
-        })
-             .then((response) => {
-                alert('Usuário criado com sucesso!')
-            })
-            .catch((error) => {
-                console.log(error.response.data)
-            })
-    }
-
     render() {
-
-        const novoCadastro = this.state.cadastros.map((cad)=>{
-            return <p>{cad.name} - {cad.email}</p>
-        })
 
         return (
             <div>
-                {novoCadastro}
-                <input> Nome:</input> 
-                <button onClick={this.cadastraUser()}>Adicionar</button> 
-                <input>E-mail:</input>
-                
+                <div>
+                    <input value={this.props.valorInputName} 
+                    onChange={this.props.onChangeUserName} 
+                    placeholder='digite seu nome'/>
+
+                    <input value={this.props.valorInputEmail} 
+                    onChange={this.props.onChangeUserEmail} 
+                    placeholder='digite e-mail'/>
+
+                    <button onClick={this.props.cadastraUser}>Adicionar</button>
+                </div>
+
             </div>
+        
         );
     }
 }
