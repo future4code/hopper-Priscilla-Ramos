0c490cd98c84552ff09a0b7c5e934dadd5a8825b
PriscillaLucena <prilucenaoliveira@gmail.com>
2022-09-21 18:39:06 -0300

    finalizei o desafio voltando os usuários com suas compras em uma propriedade purchase.

--- modulo5/labecomerce/src/endpoints/searchAllUsers.ts
+++ modulo5/labecomerce/src/endpoints/searchAllUsers.ts
@@ -6,20 +6,38 @@ export const searchAllUsers = async (req: Request, res: Response): Promise<void>
     let errorCode = 400
     try {
 
-        const users: any[] = await connection.raw(`
-            SELECT * from labecommerce_users 
+        const purchase: any[] = await connection.raw(`
+        SELECT * from labecommerce_users 
             JOIN labecommerce_purchases ON labecommerce_users.id = labecommerce_purchases.user_id
-         `);
+        `);
 
-        if (users.length < 1) {
+        const purchases: any = purchase[0].map(toPurchase)
+
+        if (purchase.length < 1) {
             errorCode = 404
             throw new Error("Não existem usuários cadastrados!");
         }
 
-        res.status(200).send(users[0])
+        res.status(200).send(purchases)
 
     } catch (error: any) {
         console.log(error)
         res.send(error.message || error.sqlMessage)
+    }
+};
+
+
+const toPurchase = (input: any): any => {
+    return {
+        id: input.id,
+        name: input.name,
+        email: input.email,
+        password: input.password,
+        userId: input.user_id,
+        purchase: {
+            productId: input.product_id,
+            quantity: input.quantity,
+            total_price: input.price
+        }
     }
 };
